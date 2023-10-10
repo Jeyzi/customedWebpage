ab4394f455af6845a53248aedb848d1ec40d30d6
jcamille <jcamille@gulfcustody.com>
2023-10-10 12:26:45 +0300

    Using SQLAlchemy to store taskList.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-import os, random, datetime, requests,openpyxl
-import pandas as pd
-import matplotlib.pyplot as plt
-from flask import Flask, render_template
+import os
+import random
+import datetime
+import requests
+from flask import Flask, request, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'  # Use SQLite database
+db = SQLAlchemy(app)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(255), nullable=False)
 
 # Define a folder containing your images
 IMAGE_FOLDER = 'static/images/'
@@ -48,7 +57,28 @@ def index():
     author = "~" + get_author()
     return render_template('index.html', image_path=image_path, greeting=greeting, quote=quote, author=author)
 
+@app.route('/add_task', methods=['POST'])
+def addTask():
+    data = request.get_json()
+    if 'text' in data:
+        task_text = data['text']
+        new_task = Task(text=task_text)
+        db.session.add(new_task)
+        db.session.commit()
+        return jsonify(success=True)
+    return jsonify(success=False)
+@app.route('/get_tasks')
+def loadTask():
+    tasks = Task.query.all()
+    task_texts = [task.text for task in tasks]
+    return jsonify(task_texts)
+
+# @app.route('/update_tasks', methods=['POST'])
+# def saveData():
+
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,125 +1,152 @@
-const openModal = document.getElementById('openModal');
+//const openModal = document.getElementById('openModal');
 
-openModal.addEventListener('click', function() {
-    loadTask();
-});
+//openModal.addEventListener('click', function() {
+//    loadTask();
+//});
 
 const inputBox = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
+// Function to create a new task element
+function createTaskElement(taskText) {
+    const listItem = document.createElement("li");
+    listItem.textContent = taskText;
 
-function addTask(){
-    if(inputBox.value == ''){
-        alert('You must write something!');
-    }
-    else{
-        let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
-        taskList.appendChild(li);
-        let span = document.createElement('span');
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = '';
-    saveData();
+    // Create a close button for the task
+    const closeBtn = document.createElement("span");
+    closeBtn.textContent = "\u00D7";
+    closeBtn.className = "close";
+    closeBtn.addEventListener("click", function () {
+        listItem.remove();
+    });
+
+    // Append the close button to the task
+    listItem.appendChild(closeBtn);
+
+    return listItem;
 }
 
+function addTask() {
+    const taskText = inputBox.value.trim();
+    if (taskText === '') {
+        alert('You must write something!');
+    } else {
+        fetch('/add_task', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: taskText }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                loadTask();
+            } else {
+                alert('Failed to add task');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+    inputBox.value = '';
+}
 
 taskList.addEventListener('click', function(e){
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle('checked'); //Modify css for class checked
-        saveData();
     }
     else if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove();
-        saveData();
     }
 }, false);
 
-//save data to local storage
-function saveData(){
-    localStorage.setItem('data', taskList.innerHTML);
+function loadTask() {
+    fetch('/get_tasks')
+        .then(response => response.json())
+        .then(data => {
+            taskList.innerHTML = ''; // Clear the task list
+            data.forEach(taskText => {
+                const listItem = createTaskElement(taskText);
+                taskList.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
+
+
+
+
+
+////save data to local storage
+//function saveData(){
+//    localStorage.setItem('data', taskList.innerHTML);
+//}
 //
-function loadTask(){
-    taskList.innerHTML = localStorage.getItem('data');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        // Function to create a new task element
-//        function createTaskElement(taskText) {
-//            const listItem = document.createElement("li");
-//            listItem.textContent = taskText;
-//
-////            // Create a close button for the task
-////            const closeBtn = document.createElement("span");
-////            closeBtn.textContent = "\u00D7";
-////            closeBtn.className = "close";
-////            closeBtn.addEventListener("click", function () {
-////                listItem.remove();
-////                saveTasksToLocalStorage();
-////            });
 ////
-////            // Append the close button to the task
-////            listItem.appendChild(closeBtn);
-//
-//            return listItem;
-//        }
-//
-//        // Function to add a task to the list
-//        function addTask() {
-//            const taskInput = document.getElementById("taskInput");
-//            const taskList = document.getElementById("taskList");
-//            const taskText = taskInput.value.trim();
-//
-//            if (taskText !== "") {
-//                const listItem = createTaskElement(taskText);
-//
-//                // Create a close button for the task
-//                const closeBtn = document.createElement("span");
-//                closeBtn.textContent = "\u00D7";
-//                closeBtn.className = "close";
-//                closeBtn.addEventListener("click", function () {
-//                    listItem.remove();
-//                    saveTasksToLocalStorage();
-//                });
-//
-//                // Append the close button to the task
-//                listItem.appendChild(closeBtn);
-//
-//                taskList.appendChild(listItem);
-//                taskInput.value = "";
-//                saveTasksToLocalStorage();
-//            }
-//        }
+//function loadTask(){
+//    taskList.innerHTML = localStorage.getItem('data');
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Function to add a task to the list
+        function addTask() {
+            const taskInput = document.getElementById("taskInput");
+            const taskList = document.getElementById("taskList");
+            const taskText = taskInput.value.trim();
+
+            if (taskText !== "") {
+                const listItem = createTaskElement(taskText);
+
+                // Create a close button for the task
+                const closeBtn = document.createElement("span");
+                closeBtn.textContent = "\u00D7";
+                closeBtn.className = "close";
+                closeBtn.addEventListener("click", function () {
+                    listItem.remove();
+                    saveTasksToLocalStorage();
+                });
+
+                // Append the close button to the task
+                listItem.appendChild(closeBtn);
+
+                taskList.appendChild(listItem);
+                taskInput.value = "";
+                saveTasksToLocalStorage();
+            }
+        }
 //
 //
 //        // Event listener to add a task when the "Add" button is clicked
